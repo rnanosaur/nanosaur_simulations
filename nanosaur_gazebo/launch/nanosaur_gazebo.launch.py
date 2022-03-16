9d5862bcc0c99d06ee2468f9f13d80a51908f5fb
Raffaello Bonghi <rbonghi@nvidia.com>
2022-03-16 22:14:15 +0000

    Add spawn entity

--- nanosaur_gazebo/launch/nanosaur_gazebo.launch.py
+++ nanosaur_gazebo/launch/nanosaur_gazebo.launch.py
@@ -24,21 +24,53 @@
 # EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import os
+import xacro
 
+from ament_index_python.packages import get_package_prefix
 from ament_index_python.packages import get_package_share_directory
 from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
 from launch.actions import ExecuteProcess
 from launch import LaunchDescription
 
 def generate_launch_description():
+    package_description = get_package_share_directory('nanosaur_description')
     package_gazebo = get_package_share_directory('nanosaur_gazebo')
     
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
-    
+
+    xacro_file = "nanosaur.urdf.xml"
     world_file_name = "empty_world.world"
 
     # full  path to urdf and world file
     world = os.path.join(package_gazebo, "worlds", world_file_name)
+    xacro_path = os.path.join(package_description, "urdf", xacro_file)
+
+    # process urdf contents because to spawn an entity in
+    # gazebo we need to provide entire urdf as string on  command line
+    robot_desc = xacro.process_file(xacro_path)
+    xml = robot_desc.toxml()
+    print(xml)
+
+    # Launch Robot State Publisher
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'use_sim_time': use_sim_time, 'robot_description': xml}]
+    )
+
+    # Spawn robot
+    spawn_robot = Node(
+        package='gazebo_ros',
+        node_executable='spawn_entity.py',
+        node_name='spawn_entity',
+        output='screen',
+        arguments=['-entity',
+                   'nanosaur',
+                   '-x', '0', '-y', '0', '-z', '0',
+                   '-topic', '/robot_description'
+                   ]
+    )
 
     # start gazebo, notice we are using libgazebo_ros_factory.so instead of libgazebo_ros_init.so
     # That is because only libgazebo_ros_factory.so contains the service call to /spawn_entity
@@ -46,11 +78,11 @@ def generate_launch_description():
         cmd=["gazebo", "--verbose", world, "-s", "libgazebo_ros_factory.so"],
         output="screen",
     )
-    
-    
 
     ld = LaunchDescription()
-    ld.add_action(gazebo_process)
+    #ld.add_action(gazebo_process)
+    #ld.add_action(robot_state_publisher_node)
+    #ld.add_action(spawn_robot)
     
     return ld
 # EOF
