99379bfc7c7e6886264b4aa291a399e4a2bb96aa
Raffaello Bonghi <rbonghi@nvidia.com>
2022-03-17 20:05:37 +0000

    improve gazebo launcher

--- nanosaur_gazebo/launch/nanosaur_gazebo.launch.py
+++ nanosaur_gazebo/launch/nanosaur_gazebo.launch.py
@@ -27,25 +27,34 @@ import os
 import xacro
 
 from ament_index_python.packages import get_package_share_directory
+from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
-from launch.actions import ExecuteProcess
 from launch import LaunchDescription
+from launch.conditions import IfCondition
+
 
 def generate_launch_description():
     package_gazebo = get_package_share_directory('nanosaur_gazebo')
     gazebo_ros_path = get_package_share_directory('gazebo_ros')
-    
+
+    world_file_name = LaunchConfiguration('world_file_name')
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
 
-    xacro_file = "nanosaur.urdf.xacro"
-    world_file_name = "empty_world.world"
+    gazebo_gui_cmd = DeclareLaunchArgument('gui', default_value='true',
+                                           description='Set to "false" to run headless.')
+
+    gazebo_server_cmd = DeclareLaunchArgument('server', default_value='true',
+                                              description='Set to "false" not to run gzserver.')
+
+    world_file_name_cmd = DeclareLaunchArgument('world_file_name', default_value='aaaaaempty_world.world',
+                                                description='Load gazebo world.')
 
     # full  path to urdf and world file
-    world = os.path.join(package_gazebo, "worlds", world_file_name)
-    xacro_path = os.path.join(package_gazebo, "urdf", xacro_file)
+    # world = os.path.join(package_gazebo, "worlds", world_file_name)
+    xacro_path = os.path.join(package_gazebo, "urdf", "nanosaur.urdf.xacro")
 
     # process urdf contents because to spawn an entity in
     # gazebo we need to provide entire urdf as string on  command line
@@ -65,28 +74,39 @@ def generate_launch_description():
         node_executable='spawn_entity.py',
         node_name='spawn_entity',
         output='screen',
-        arguments=['-entity',
-                   'nanosaur',
-                   '-x', '0', '-y', '0', '-z', '0',
-                   '-topic', '/robot_description'
+        arguments=['-entity', 'nanosaur',
+                   '-topic', '/robot_description',
+                   '-x', '0', '-y', '0', '-z', '0'
                    ]
     )
 
     # start gazebo, notice we are using libgazebo_ros_factory.so instead of libgazebo_ros_init.so
     # That is because only libgazebo_ros_factory.so contains the service call to /spawn_entity
-    gazebo_process = ExecuteProcess(
-        cmd=["gazebo", "--verbose", world, "-s", "libgazebo_ros_factory.so"],
-        output="screen",
+    # Reference options
+    # https://github.com/ros-simulation/gazebo_ros_pkgs/blob/foxy/gazebo_ros/launch/gzserver.launch.py
+    gazebo_server = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [os.path.join(gazebo_ros_path, 'launch'), '/gzserver.launch.py']),
+        launch_arguments={'world': [package_gazebo, "/worlds/", world_file_name],
+                          'verbose': 'true',
+                          'init': 'false'}.items(),
+        condition=IfCondition(LaunchConfiguration('server'))
     )
 
-    gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(gazebo_ros_path, 'launch'), '/gazebo.launch.py']),
-             )
+    gazebo_gui = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [os.path.join(gazebo_ros_path, 'launch'), '/gzclient.launch.py']),
+        condition=IfCondition(LaunchConfiguration('gui'))
+    )
 
     ld = LaunchDescription()
-    ld.add_action(gazebo)
+    ld.add_action(gazebo_gui_cmd)
+    ld.add_action(gazebo_server_cmd)
+    ld.add_action(world_file_name_cmd)
+    ld.add_action(gazebo_server)
+    ld.add_action(gazebo_gui)
     ld.add_action(robot_state_publisher_node)
     ld.add_action(spawn_robot)
-    
+
     return ld
 # EOF
