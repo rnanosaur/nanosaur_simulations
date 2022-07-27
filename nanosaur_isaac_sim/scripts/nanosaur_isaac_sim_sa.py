510e133416e682dbab76653584345b0ec1962379
Raffaello Bonghi <raffaello@rnext.it>
2022-07-27 17:06:19 +0100

    Improve IsaacSIM SA launcher

--- nanosaur_isaac_sim/scripts/nanosaur_isaac_sim_sa.py
+++ nanosaur_isaac_sim/scripts/nanosaur_isaac_sim_sa.py
@@ -32,12 +32,14 @@ CONFIG = {"renderer": "RayTracedLighting", "headless": False}
 # Example ROS2 bridge sample demonstrating the manual loading of Multiple Robot Navigation scenario
 simulation_app = SimulationApp(CONFIG)
 
+import omni.graph.core as og
 from omni.isaac.core.utils.extensions import enable_extension
 from omni.isaac.core.utils.stage import is_stage_loading
 from omni.isaac.core import World, SimulationContext
 from omni.isaac.core.utils import nucleus
 from omni.kit import commands
 from omni import usd
+from omni.isaac.core_nodes.scripts.utils import set_target_prims
 
 
 # enable ROS2 bridge extension
@@ -100,9 +102,10 @@ class IsaacWorld():
 
 class RobotLoader(Node):
     
-    def __init__(self, isaac_world):
-        super().__init__("robot_loader", namespace="nanosaur")
+    def __init__(self, isaac_world, namespace="nanosaur"):
+        super().__init__("robot_loader", namespace=namespace)
         # Load isaac_world
+        self.namespace = namespace
         self.isaac_world = isaac_world
         # setup the ROS2 subscriber here
         self.ros_sub = self.create_subscription(String, "robot_description", self.callback_description, 1)
@@ -110,7 +113,7 @@ class RobotLoader(Node):
         # Node started
         self.get_logger().info("Robot loader start")
 
-    def load_robot(self):
+    def load_robot(self, robot_name):
         # Setting up import configuration:
         status, import_config = commands.execute(
             "URDFCreateImportConfig")
@@ -130,17 +133,77 @@ class RobotLoader(Node):
         )
         # Wait a step
         self.isaac_world.wait_step_reload()
+        
+        # Creating a action graph with ROS component nodes
+        # https://docs.omniverse.nvidia.com/app_isaacsim/app_isaacsim/tutorial_ros2_python.html
+        # https://docs.omniverse.nvidia.com/app_isaacsim/app_isaacsim/tutorial_ros2_turtlebot.html#build-the-graph
+        try:
+            og.Controller.edit(
+                {"graph_path": f"/ActionGraph", "evaluator_name": "execution"},
+                {
+                    og.Controller.Keys.CREATE_NODES: [
+                        ("OnPlaybackTick", "omni.graph.action.OnPlaybackTick"),
+                        ("ROS2Context", "omni.isaac.ros2_bridge.ROS2Context"),
+                        ("ROS2SubscribeTwist", "omni.isaac.ros2_bridge.ROS2SubscribeTwist"),
+                        ("scale_to_from_stage_units", "omni.isaac.core_nodes.OgnIsaacScaleToFromStageUnit"),
+                        ("break_3_vector_01", "omni.graph.nodes.BreakVector3"),
+                        ("break_3_vector_02", "omni.graph.nodes.BreakVector3"),
+                        ("DifferentialController", "omni.isaac.wheeled_robots.DifferentialController"),
+                        ("ConstantToken_01", "omni.graph.nodes.ConstantToken"),
+                        ("ConstantToken_02", "omni.graph.nodes.ConstantToken"),
+                        ("MakeArray", "omni.graph.nodes.MakeArray"),
+                        ("IsaacArticulationController", "omni.isaac.core_nodes.IsaacArticulationController"),
+                    ],
+                    og.Controller.Keys.CONNECT: [
+                        ("OnPlaybackTick.outputs:tick", "ROS2SubscribeTwist.inputs:execIn"),
+                        ("ROS2Context.outputs:context", "ROS2SubscribeTwist.inputs:context"),
+                        ("ROS2SubscribeTwist.outputs:angularVelocity", "break_3_vector_01.inputs:tuple"),
+                        ("ROS2SubscribeTwist.outputs:linearVelocity", "scale_to_from_stage_units.inputs:value"),
+                        ("scale_to_from_stage_units.outputs:result", "break_3_vector_02.inputs:tuple"),
+                        ("ROS2SubscribeTwist.outputs:execOut", "DifferentialController.inputs:execIn"),
+                        ("break_3_vector_01.outputs:z", "DifferentialController.inputs:angularVelocity"),
+                        ("break_3_vector_02.outputs:x", "DifferentialController.inputs:linearVelocity"),
+                        ("OnPlaybackTick.outputs:tick", "IsaacArticulationController.inputs:execIn"),
+                        ("DifferentialController.outputs:velocityCommand", "IsaacArticulationController.inputs:velocityCommand"),
+                        ("ConstantToken_01.inputs:value", "MakeArray.inputs:a"),
+                        ("ConstantToken_02.inputs:value", "MakeArray.inputs:b"),
+                        ("MakeArray.outputs:array", "IsaacArticulationController.inputs:jointNames"),
+                    ],
+                    og.Controller.Keys.SET_VALUES: [
+                        # Assigning a Domain ID of 1 to Context node
+                        ("ROS2Context.inputs:domain_id", 0),
+                        # Assigning topic name to clock publisher
+                        ("ROS2SubscribeTwist.inputs:topicName", "/nanosaur/cmd_vel"),
+                        # Assigning Differential controller configuration
+                        ("DifferentialController.inputs:maxLinearSpeed", 0.5),
+                        ("DifferentialController.inputs:wheelDistance", 0.4*2),
+                        ("DifferentialController.inputs:wheelRadius", 0.0150),
+                        # Assign Articulation controller configuration
+                        ("IsaacArticulationController.inputs:usePath", False),
+                        # Assigning topic name to clock publisher
+                        ("ConstantToken_01.inputs:value", "sprocket_left_joint"),
+                        ("ConstantToken_02.inputs:value", "sprocket_right_joint"),
+                    ]
+                },
+            )
+        except Exception as e:
+            print(e)
+        
+        NANOSAUR_STAGE_PATH="/nanosaur/base_link"
+        # Setting the /Franka target prim to Subscribe JointState node
+        set_target_prims(primPath="/ActionGraph/IsaacArticulationController", targetPrimPaths=[NANOSAUR_STAGE_PATH])
 
     def callback_description(self, msg):
         # callback function to set the cube position to a new one upon receiving a (empty) ROS2 message
         print(f"Load robot")
         robot_urdf = msg.data
-        print(robot_urdf)
+        #print(robot_urdf)
+        robot_name = "nanosaur"
         text_file = open(PATH_LOCAL_URDF_FOLDER, "w")
         n = text_file.write(robot_urdf)
         text_file.close()
         # Load robot
-        self.load_robot()
+        self.load_robot(robot_name)
 
 
 if __name__ == "__main__":
